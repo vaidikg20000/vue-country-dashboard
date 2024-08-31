7e87b3446867217750c6aa4eb4954b47f0eda78b
Vaidik Gupta <vaidikgupta2000@gmail.com>
2024-08-31 20:44:19 +0530

    feat: changed title and favicon image

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -37,22 +37,5 @@ const closeModal = () =>{
 }
 
 
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
 
-.modal-content {
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-}
 </style>
